d8bdcd4fd78f810313d793a04e41b8ca6aeefc85
Saeko Onishi <onishi.saeko@asuene.com>
2024-09-30 00:13:50 +0900

    feat: Add example lang file

---
+++ lang/en/messages.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'logout' => 'logout',
+
+    'header' => [
+        'switch' => [
+            'show_popup' => 'switch',
+        ],
+    ],
+
+    // 共通メニュー
+    'menu' => [
+        'dashboard' => 'dashboard',
+    ],
+
+    // ダッシュボード
+    'dashboard' => [
+    ]
+];
---
+++ lang/ja/messages.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'logout' => 'ログアウト',
+
+    'header' => [
+        'switch' => [
+            'show_popup' => '切り替え',
+        ],
+    ],
+
+    // 共通メニュー
+    'menu' => [
+        'dashboard' => 'ダッシュボード',
+        'analysis' => [
+            'title' => '解析',
+        ],
+    ],
+
+    // ダッシュボード
+    'dashboard' => [
+        'nocontract' => '契約がありません。<br/>「お問い合せ」からご連絡ください。',
+    ]
+];
+?>
